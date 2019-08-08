efd741fabba9b1f996cbb98375a2e3855461e136
Mutinan Phaohing <mutinan.p@linecorp.com>
2019-08-08 18:17:21 +0700

    fix filter bug

--- pages/strings/[id].js
+++ pages/strings/[id].js
@@ -7,13 +7,16 @@ import { FixedSizeList as List, areEqual } from 'react-window'
 import memoize from 'memoize-one'
 import styled from 'styled-components'
 
+const hasWindow = typeof window !== 'undefined'
+
 const Wrapper = styled.div`
   width: 1200px;
   margin: auto;
 `
 
 const StatusBar = styled.div`
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Tahoma";
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Tahoma';
   padding: 10px;
   border-bottom: 1px solid #e0e0e0;
   position: relative;
@@ -42,18 +45,37 @@ const FilterWrapper = styled.div`
   padding: 10px 0px;
 `
 
+const TextSearch = styled.input`
+  padding: 5px 10px;
+  border: 1px solid #e2e2e2;
+  margin-top: 2px;
+  box-sizing: border-box;
+  font-size: 12px;
+  border-radius: 5px;
+  margin-left: 10px;
+`
+
 const Row = memo(({ data, index, style }) => {
-  const { db, changes } = data
+  const { db, changes, setChanges } = data
   const { $: attr, _: value } = db[index]
   const { Key: key, Original: original, _index: recordIndex } = attr
   const isTranslated = typeof original === 'string'
 
   const callback = text => {
-    changes.set(recordIndex, text)
+    changes[recordIndex] = { text, index }
+    setChanges({ ...changes })
   }
 
+  const change = changes[recordIndex] || { text: '', index }
+
   return (
-    <div style={{...style, backgroundColor: isTranslated ? '#99cef5' : 'transparent', overflow: 'hidden' }}>
+    <div
+      style={{
+        ...style,
+        backgroundColor: isTranslated ? '#99cef5' : 'transparent',
+        overflow: 'hidden'
+      }}
+    >
       {isTranslated ? (
         <TranslatedRow
           index={`${key}-${recordIndex}`}
@@ -65,14 +87,13 @@ const Row = memo(({ data, index, style }) => {
         <UntranslatedRow
           index={`${key}-${recordIndex}`}
           original={value}
-          translation={changes.get(recordIndex) || ''}
+          translation={change.text}
           onChange={callback}
         />
       )}
     </div>
   )
 }, areEqual)
-
 
 const postback = (id, db) => {
   fetch(`http://localhost:3000/api/save?file=${id}`, {
@@ -84,66 +105,102 @@ const postback = (id, db) => {
   })
 }
 
-const pack = memoize((db, changes, filter) => {
-  if (filter) {
-    return { db: db.filter(r => !r.$.Original), changes }
-  }
-  return { db, changes }
+const pack = memoize((db, changes, setChanges) => {
+  return { db, changes, setChanges }
 })
 
-
-const hasWindow = typeof window !== 'undefined'
+const applyFilter = (filter, data) => {
+  return data.filter(r => {
+    const o = (r.$.Original || r._).toLowerCase()
+    return (filter.onlyUntranslate ? !r.$.Original : true) 
+    && (filter.text.length ? o.includes(filter.text) : true) 
+  })
+}
 
 // Component
-const Translate = ({ data, initPage = 1 }) => {
+const Translate = ({ data }) => {
   const router = useRouter()
-  const { id } = router.query
+  //const { id } = router.query
 
-  data.map((data, i) => {
-    data.$._index = i
+  const [database, setDatabase] = useState(data)
+  const [changes, setChanges] = useState({})
+  const [viewPortHeight, setViewPortHeight] = useState(
+    hasWindow ? window.innerHeight - 80 : 800
+  )
+  const [filter, setFilter] = useState({
+    onlyUntranslate: false,
+    text: ''
   })
-
-  const [db, setDb] = useState(data)
-  const [viewPortHeight, setViewPortHeight] = useState(hasWindow ? (window.innerHeight - 80) : 800)
-  const [onlyUntranslated, setOnlyUntranslated] = useState(false)
-  const changes = new Map()
+  const [displayData, setDisplayData] = useState(database)
 
   if (hasWindow) {
-    window.addEventListener('resize', () => { setViewPortHeight(window.innerHeight - 80) })
+    window.addEventListener('resize', () => {
+      setViewPortHeight(window.innerHeight - 80)
+    })
   }
 
   const save = () => {
-    console.log('changes', changes.entries())
-    changes.forEach((text, index) => {
-      const record = db[index]
+    new Map(Object.entries(changes)).forEach(({ text, index }, recordIndex) => {
+      const record = database[recordIndex]
       if (record.$.Original) {
-        db[index] = { $: { Key: record.$.Key, Original: record.$.Original }, _: text }
+        database[recordIndex] = {
+          $: { Key: record.$.Key, Original: record.$.Original },
+          _: text
+        }
       } else {
-        db[index] = { $: { Key: record.$.Key, Original: record._ }, _: text }
+        database[recordIndex] = {
+          $: { Key: record.$.Key, Original: record._ },
+          _: text
+        }
       }
+      displayData[index] = { ...database[recordIndex],  }
     })
-    changes.clear()
-    setDb([...db])
-    //postback(id, db)
+    setChanges({})
+    setDatabase([...database])
+    setDisplayData([...displayData])
+    postback(id, database)
   }
 
-  const total = db.length
-  const translatedCount = db.filter(r => r.$.Original).length
-  const percentage = translatedCount / total * 100
-  const items = pack(db, changes, onlyUntranslated)
-
+  const total = data.length
+  const translatedCount = data.filter(r => r.$.Original).length
+  const percentage = (translatedCount / total) * 100
 
   const handleFilterChange = e => {
-    setOnlyUntranslated(e.target.checked)
+    const nextFilter = {
+      ...filter,
+      onlyUntranslate: e.target.checked,
+    }
+    setFilter(nextFilter)
+    setDisplayData(applyFilter(nextFilter, database))
   }
 
-  console.log('rerender', items.db.length)
+  const handleTextFilterChange = e => {
+    const val = e.target.value.toLowerCase()
+    const nextFilter = {
+      ...filter,
+      text: val
+    }
+    setFilter(nextFilter)
+    setDisplayData(applyFilter(nextFilter, database))
+  }
+
+  const items = pack(displayData, changes, setChanges)
+
   return (
     <Wrapper>
       <StatusBar>
-        <div>Status translated: ({translatedCount}/{total}) {percentage}%</div>
+        <div>
+          Status translated: ({translatedCount}/{total}) {percentage}%
+        </div>
         <FilterWrapper>
-          <Checkbox id="filter-untranslate" type="checkbox" checked={onlyUntranslated} onChange={handleFilterChange} /><label htmlFor="filter-untranslate" >Only untranslate</label>
+          <Checkbox
+            id="filter-untranslate"
+            type="checkbox"
+            checked={filter.onlyUntranslate}
+            onChange={handleFilterChange}
+          />
+          <label htmlFor="filter-untranslate">Only untranslate</label>
+          <TextSearch type="text" onChange={handleTextFilterChange} />
         </FilterWrapper>
         <SaveBtn onClick={save}>Save</SaveBtn>
       </StatusBar>
@@ -165,7 +222,14 @@ Translate.getInitialProps = async ctx => {
     `http://localhost:3000/api/load?file=${ctx.query.id}`
   )
   const json = await data.json()
-  return { data: json.list }
+
+  return {
+    data: json.list.map((record, i) => {
+      record.$._index = i
+      if (!record._) { record._ = '' }
+      return record
+    })
+  }
 }
 
 export default Translate
